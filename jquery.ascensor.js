7ca72dedb8f0d8ae8fe90ff8926347d261fbf0d8
léo galley <contact@kirkas.ch>
2012-10-18 13:09:46 +0200

    hashchange & animation queued
    Adding hashchange fonction, making the previous event scroll in
    different section (if they have a hash option).
    
    Addind queued in scrollAnimation (x or y)

--- jquery.ascensor.js
+++ jquery.ascensor.js
@@ -1,10 +1,9 @@
 /*********************************************************************************************************
 
     JQUERY.ASCENSOR.JS
-        VERSION: 1.4.2
+        VERSION: 1.5
         DATE: 9/10/2012
-
-
+        
     INDEX
         1. PLUGIN DEFAULTS OPTIONS
         2. NODE OPTIONS DEFINITIONS
@@ -39,7 +38,9 @@
             AscensorFloorName: "",
             Time: "1000",
             Easing: "linear",
-            KeyNavigation: true
+            KeyNavigation: true,
+            Queued:false,
+            QueuedDirection:"x"
         };
 
 
@@ -53,37 +54,37 @@
         this._name = pluginName;
         this.init();
     }
-
+    
 
     Plugin.prototype.init = function () {
 
-
+        
         /***********************************************************************
             3. SETTINGS
         ***********************************************************************/
-
+        
         //element settings
         var self = this;
         var node = this.element;
         var nodeChildren = $(node).children(self.options.ChildType);
-
+        
         //stage counter settings
         var StageOn = self.options.WindowsOn;
         var StageNumber = 0;
-
+        
         //height/width settings
         var WW;
         var WH;
-
+        
         //plugins settings
         var floorXY = self.options.AscensorMap.split(" & ");
         var floorName = self.options.AscensorFloorName.split(" | ");
         var direction = self.options.Direction;
-
+        
         //hash var
         var hash = window.location.hash.split("/").pop();
 
-
+        
         /***********************************************************************
             4. HASH ANALYSIS
         ***********************************************************************/
@@ -97,14 +98,30 @@
                 }
             });
         }
+        
 
+        $(window).on("hashchange",function(){
+	        if (self.options.AscensorFloorName !== null) {
+		        hash=window.location.hash.split("/").pop();
+		        $(floorName).each(function (index) {
+                	if (hash === floorName[index]) {
+                    	StageOn = index + 1;
+                    	$("." + self.options.AscensorName + "Link")
+                        	.eq(StageOn - 1)
+                        	.addClass(self.options.AscensorName + "LinkActive");
+                        targetScroll(StageOn, self.options.Time) 
+                    }
+            });
+	        }
+        })
 
+       
 
         /***********************************************************************
             5. RESIZE FONCTION
         ***********************************************************************/
         function elementResize() {
-
+            
             WW = $(window).width();
             WH = $(window).height();
 
@@ -124,7 +141,7 @@
 
             if (direction === "chocolate") {
                 var target = floorXY[StageOn - 1].split("|");
-
+                
                 $(nodeChildren).each(function (index) {
                     var CoordName = floorXY[index].split("|");
                     $(this).css({"position": "absolute", "left": (CoordName[1] - 1) * WW, "top": (CoordName[0] - 1) * WH}); 
@@ -138,17 +155,107 @@
             6. SCROLLTO FONCTION
         ***********************************************************************/
         function targetScroll(floor, time) {
+
+        
             if (direction === "y") {
-                $(node).stop().animate({scrollTop: (floor - 1) * $(window).height()},time,self.options.Easing);
+                $(node)
+                	.stop()
+                	.animate(
+                		{scrollTop: (floor - 1) * WH},
+                		time,
+                		self.options.Easing
+                	);
             }
 
             if (direction === "x") {
-                $(node).stop().animate({scrollLeft: (floor - 1) * $(window).width()},time,self.options.Easing);
+                $(node)
+                	.stop()
+                	.animate(
+                		{scrollLeft: (floor - 1) * WW},
+                		time,
+                		self.options.Easing
+                	);
             }
+            
+        
 
             if (direction === "chocolate") {
-                var target = floorXY[floor - 1].split("|");
-                $(node).stop().animate({scrollLeft: (target[1] - 1) * $(window).width(),scrollTop: (target[0] - 1) * $(window).height()},time,self.options.Easing);
+            
+            	var target = floorXY[floor - 1].split("|");
+            	var floorReference = floorXY[StageOn - 1].split("|");
+            	
+            	if(self.options.Queued){
+            		if(self.options.QueuedDirection==="x"){
+            		
+            			if($(node).scrollLeft()===(target[1] - 1) * WW){
+	     					$(node)
+	     						.stop()
+	     						.animate(
+	     							{scrollTop: (target[0] - 1) * WH},
+	     							time,
+	     							self.options.Easing
+	     						);	
+     					}else{
+	     					$(node)
+	     						.stop()
+	     						.animate(
+	     							{scrollLeft: (target[1] - 1) * WW},
+	     							time,
+	     							self.options.Easing,
+	     							function(){
+		     							$(node)
+		     								.stop()
+		     								.animate(
+		     									{scrollTop: (target[0] - 1) * WH},
+		     									time,
+		     									self.options.Easing
+		     								);
+		     						}
+		     					);
+     					}
+     					
+            		}else if(self.options.QueuedDirection==="y"){
+            		
+	            		if($(node).scrollTop()===(target[0] - 1) * WH){
+	     					$(node)
+	     						.stop()
+	     						.animate(
+	     							{scrollLeft: (target[1] - 1) * WW},
+	     							time,
+	     							self.options.Easing
+	     						)
+     					}else{
+	     					$(node)
+	     						.stop()
+	     						.animate(
+	     							{scrollTop: (target[0] - 1) * WH},
+	     							time,
+	     							self.options.Easing,
+	     							function(){
+		     							$(node)
+		     								.stop()
+		     								.animate(
+		     									{scrollLeft: (target[1] - 1) * WW},
+		     									time,
+		     									self.options.Easing
+		     								);
+		     						}
+		     				);
+     					}
+     					
+            		}
+            		
+            	}else{
+	            	$(node)
+	            		.stop()
+	            		.animate(
+	            			{scrollLeft: (target[1] - 1) * WW,scrollTop: (target[0] - 1) * WH},
+	            			time,
+	            			self.options.Easing
+	            		);
+            	}
+                
+                
             }
 
             if (self.options.AscensorFloorName !== null) {
@@ -193,6 +300,7 @@
                 }
             }
 
+            
             if (direction === "chocolate") {
                 var floorReference = floorXY[StageOn - 1].split("|");
                 $.each(floorXY, function (index) {
@@ -279,7 +387,7 @@
                 $(document).keydown(checkKey);
             }
         }
-
+        
 
         /***********************************************************************
             11. MOBILE ROTATION DETECTION
@@ -304,7 +412,7 @@
         $(window).resize(function () {
             elementResize();
         }).resize();
-
+    
         elementResize();
         targetScroll(StageOn, 1)
     };
