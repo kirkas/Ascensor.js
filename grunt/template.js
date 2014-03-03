ba71fa324cdef21e55934e0fd04818a53f576a3c
léo galley <contact@kirkas.ch>
2014-03-02 19:36:51 -0800

    update hash

--- grunt/template.js
+++ grunt/template.js
@@ -32,7 +32,7 @@ module.exports = function(grunt) {
       dest: 'examples/example_url.html',
       variables: {
         title: 'Chocolat',
-        params: '{ascensorFloorName:["Home", "Implementation", "HTML" , "Jquery" , "CSS", "Smartphone", "End", "Yaaay"], direction: [[0,0],[0,1],[0,2],[1,2],[1,3],[1,4],[2,4],[2,5]]}'
+        params: '{ascensorFloorName:["Home", "Implementation", "HTML" , "Jquery" , "CSS", "Smartphone", "End", "Yaaay"], direction: [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,0],[2,1]]}'
       }
     },
 
